fbd637ee48259dcd52510660f0f383fd64da164e
Bxxxlscsass <fzi45x340@gmail.com>
2026-01-11 20:32:20 -0300

    Add files via upload

---
+++ GameTest/Principal.java
@@ -0,0 +1,11 @@
+package empresa;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Cria uma instancia do jogo e inicia a execução
+		JogoDaForca jogo = new JogoDaForca();
+		jogo.iniciar();
+	}
+
+}
